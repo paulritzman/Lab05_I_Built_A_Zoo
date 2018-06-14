935711af78ffc208dfa57e35c9e9f55c89d2cd61
paulritzman <ritzman.paul@gmail.com>
2018-06-13 20:56:27 -0700

    Created abstract Animal Class

---
+++ BuildAZoo/BuildAZoo/Classes/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildAZoo.Classes
+{
+    abstract class Animal
+    {
+        public abstract int NumberOfEyes { get; set; }
+
+        public abstract string Diet { get; set; }
+
+        public abstract string BodyCovering { get; set; }
+
+        public virtual string Sound { get; set; } = "RAWR";
+
+        public virtual string Name { get; set; } = "Frank";
+
+        public abstract string Eat();
+
+        public abstract string LookAround();
+
+        public virtual string MakeSound(string sound)
+        {
+            return $"I go \"{sound}\"!";
+        }
+    }
+}
