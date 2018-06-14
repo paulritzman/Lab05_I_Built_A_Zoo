81552d2386c3c411cc382f1830e64855df9191f7
paulritzman <ritzman.paul@gmail.com>
2018-06-13 21:46:32 -0700

    Added abstract Bat and concrete Batman and VampireBat Classes

---
+++ BuildAZoo/BuildAZoo/Classes/Batman.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildAZoo.Classes
+{
+    public class Batman : Bat
+    {
+        public override string BodyCovering { get; set; } = "cape";
+
+        public override string Diet { get; set; } = "Justice";
+
+        public override string Sound { get; set; } = "I'm Batman";
+    }
+}
---
+++ BuildAZoo/BuildAZoo/Classes/VampireBat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildAZoo.Classes
+{
+    public class VampireBat : Bat
+    {
+        public override string Diet { get; set; } = "blood";
+
+        public override string Sound { get; set; } = "swoosh";
+    }
+}
