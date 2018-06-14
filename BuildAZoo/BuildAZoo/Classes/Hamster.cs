d515feda302674b3b295ed03473f6e7e3d573681
paulritzman <ritzman.paul@gmail.com>
2018-06-13 21:54:52 -0700

    Added abstract Rodent and concrete Hamster Classes

---
+++ BuildAZoo/BuildAZoo/Classes/Hamster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildAZoo.Classes
+{
+    public class Hamster : Rodent
+    {
+        public override string Diet { get; set; } = "crickets";
+
+        public override string Sound { get; set; } = "squeak";
+
+        public bool WasYourMother = true;
+    }
+}
