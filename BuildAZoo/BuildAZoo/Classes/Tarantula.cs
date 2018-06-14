57379d368cab4de67f01da1556ad3bdc7142b667
paulritzman <ritzman.paul@gmail.com>
2018-06-13 21:33:05 -0700

    Added concrete BlackWidow and Tarantula Classes

---
+++ BuildAZoo/BuildAZoo/Classes/Tarantula.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildAZoo.Classes
+{
+    public class Tarantula : Spider
+    {
+        public override string Diet { get; set; } = "rodents";
+
+        public override string BodyCovering { get; set; } = "fur";
+    }
+}
