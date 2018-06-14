1f6617eaee151a116772a2b1ab8ab71e3e8800e3
paulritzman <ritzman.paul@gmail.com>
2018-06-13 21:28:37 -0700

    Created abstract Bug and concrete Centipede Classes

---
+++ BuildAZoo/BuildAZoo/Classes/Centipede.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildAZoo.Classes
+{
+    public class Centipede : Insect
+    {
+        public override bool IsCreepyCrawly { get; set; } = true;
+
+        public override string Diet { get; set; } = "rodents";
+
+        public override string CrawlOutOfWall()
+        {
+            return "I come in from the garden, squeezing under your back door.";
+        }
+    }
+}
