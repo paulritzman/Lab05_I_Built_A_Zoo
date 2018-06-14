e7f37f5de963426d34195cca1078156137fd7535
paulritzman <ritzman.paul@gmail.com>
2018-06-13 21:41:09 -0700

    Added abstract Lizard and concrete Iguana Classes

---
+++ BuildAZoo/BuildAZoo/Classes/Iguana.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildAZoo.Classes
+{
+    public class Iguana : Lizard
+    {
+        public override string Diet { get; set; } = "vegetables";
+
+        public override string Sound { get; set; } = "braaah";
+    }
+}
