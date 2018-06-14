106b9338d68558d1c117c2fc345ffd9f3a8edae8
paulritzman <ritzman.paul@gmail.com>
2018-06-13 21:36:20 -0700

    Created abstract Snake and concrete RattleSnake Classes

---
+++ BuildAZoo/BuildAZoo/Classes/RattleSnake.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildAZoo.Classes
+{
+    public class RattleSnake : Snake
+    {
+        public override string Sound { get; set; } = "rattle";
+    }
+}
